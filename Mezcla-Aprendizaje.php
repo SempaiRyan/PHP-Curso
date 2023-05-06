c082d5ee7b4e79ef69ba33288a6e9eea540f35e2
Carlos <garciajuegandoconxbox@gmail.com>
2023-05-06 16:53:21 -0500

    Mezcla HTML Y PHP

---
+++ Mezcla-Aprendizaje.php
@@ -0,0 +1,23 @@
+<?php
+    print "<p>Inicio  HTML-PHP </p>";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>HTML-PHP</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+
+<body>
+    <?php print "<p> Hola Mezcla en Body :D </p>"; ?>
+
+</body>
+
+</html>
+<?php print "<p> <h1>Fin de la Mezcla amigos :,c </p> </h1>"; ?>
---
+++ Mezcla-Html-Php.php
@@ -0,0 +1,28 @@
+<?php
+    print "<p>Inicio del Programa </p>";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+
+    <title><?php print "Mezclar PHP Y HTML"; ?> </title>
+
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+
+    <script src='main.js'></script>
+</head>
+
+<body>
+    <?php print "<h1> Esto es el Titulo de Body </h1>"; ?>
+    <input type="text" name="texto" value="<?php print 'Valor Inicial'; ?>">
+</body>
+
+</html>
+<?php print "<p> Fin del Programa </p>"; ?>
