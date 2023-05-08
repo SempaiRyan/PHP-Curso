1eea8fcf8fcfab8471b617c7fbaad8192c4f04fa
Carlos <garciajuegandoconxbox@gmail.com>
2023-05-08 15:59:44 -0500

    Mejorando Documentacion.php

--- Documentacion05.php
+++ Documentacion05.php
@@ -2,14 +2,7 @@
     echo "Comentario PHP DOC";
 
 #clase
-class ClassName extends AnotherClass implements Interface
-{
-    fuction __construct(argument)
-    {
-        //code
-    }
-    
-}
-//Esto es un mensaje recordar ver tutos
+
+//Esto es un mensaje recordar ver tutos Salud2
 
 ?>
